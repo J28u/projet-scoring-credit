cdc18e0e5a25f305e8dc0289d691634762ebdd99
Hermine Assossou <hermineassossou@gmail.com>
2023-06-29 04:08:05 +0200

    ajout dockerfile

---
+++ api/app/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:345aa74b9ff4b4eaa847e581115735ccc23fcd20c78b525bb02c46972ffb8df3
+size 6421
--- dashboard/functions.py
+++ dashboard/functions.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a434fb83e73c9b3eda57fb10b9ce31119848e5593af288d4b69b2239afca963e
-size 15507
+oid sha256:d157c6a15285136ba8e06123cda2011a2bed7feed2612b0fe3cfed4eca4e8e37
+size 16224
