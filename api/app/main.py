fb6a0636c37853b2b0756e0a6145d183e911a484
Hermine Assossou <hermineassossou@gmail.com>
2023-07-01 15:14:22 +0200

    re-upload .py files

--- api/app/main.py
+++ api/app/main.py
@@ -1,3 +1,182 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:345aa74b9ff4b4eaa847e581115735ccc23fcd20c78b525bb02c46972ffb8df3
-size 6421
+import __main__
+from warnings import simplefilter
+import dill
+import pandas as pd
+import shap
+import uvicorn
+import json
+import os
+from fastapi import FastAPI, Response
+from fastapi.responses import FileResponse
+from fastapi.params import Query
+import logging
+
+__main__.pd = pd
+
+app = FastAPI(title='DefaultRiskApp',
+              description="""API qui alimente le tableau de bord client, construit dans le cadre du 
+              projet 7 du parcours Datascientist proposé par OpenClassrooms""",
+              version="0.1")
+
+my_logger = logging.getLogger()
+my_logger.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, filename='logs.log')
+
+DATA_PATH = os.environ.get('DATA_PATH')
+MODEL_PATH = os.environ.get('MODEL_PATH')
+
+simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
+
+classifier = None
+nearest_neighbors = None
+
+client_database = pd.DataFrame()
+best_params = pd.DataFrame()
+
+
+@app.on_event("startup")
+def load_models():
+    global classifier
+    global nearest_neighbors
+    my_logger.info('start loading models')
+    try:
+        classifier = dill.load(open(MODEL_PATH + "/classifier.pkl", "rb"))
+        nearest_neighbors = dill.load(open(MODEL_PATH + "/nearest_neighbors.pkl", "rb"))
+        my_logger.info('Models loaded')
+    except BaseException as e:
+        my_logger.error("Error while trying to load models : " + str(e))
+        
+
+@app.on_event("startup")
+def load_data():
+    global client_database
+    global best_params
+    my_logger.info('start loading data')
+    try:
+        client_database = pd.read_pickle(DATA_PATH + '/X_sample.pkl')
+        best_params = pd.read_pickle(DATA_PATH + '/BestParams.pkl')
+        my_logger.info('Data loaded')
+    except BaseException as e:
+        my_logger.error("Error while trying to load models : " + str(e))
+
+
+@app.get('/in_database')
+def check_client_in_database(client_id: int):
+    try:
+        check = client_id in client_database.index
+        return {'check': check}
+    except BaseException as e:
+        my_logger.error("Error while checking whether client is in file or not :" + str(e))
+
+
+@app.get('/threshold')
+def get_default_threshold():
+    try:
+        thresh = best_params.loc[best_params['Param'] == 'thresh', 'Best Param'].values[0]
+        return {"threshold": thresh}
+    except BaseException as e:
+        my_logger.error("Error while reading threshold in Best_Params.pkl file :" + str(e))
+
+
+@app.get('/client_info')
+def get_client_info(client_id: int):
+    try:
+        client_info = client_database.loc[[client_id]]
+
+        return Response(client_info.to_json(orient='records'), media_type="application/json")
+    except BaseException as e:
+        my_logger.error('Error while retrieving client info: ' + str(e))
+
+
+@app.get('/download_database')
+def download_client_database():
+    file_path = DATA_PATH + '/X_sample.pkl'
+    if os.path.exists(file_path):
+        return FileResponse(path=file_path, filename=file_path, media_type='application/pickle')
+    my_logger.error('Client database file not found')
+    return {"message": file_path + " file not found"}
+
+
+@app.get('/predict_default')
+def predict_default(client_id: int):
+    try:
+        client_info = client_database.loc[[client_id]]
+        thresh = best_params.loc[best_params['Param'] == 'thresh', 'Best Param'].values[0]
+
+        client_proba = classifier.predict_proba(client_info)
+        client_default_proba = client_proba[:, 1][0]
+
+        if client_default_proba > thresh:
+            prediction = "Crédit refusé"
+        else:
+            prediction = "Crédit accordé"
+
+        return {'prediction': prediction, 'proba_default': client_default_proba}
+    except BaseException as e:
+        my_logger.error('Error while predicting client default proba: ' + str(e))
+
+
+@app.get('/predict_default_all_clients')
+def predict_default_all(client_ids: list[int] = Query(...)):
+    try:
+        client_info = client_database.loc[client_ids]
+        client_proba = classifier.predict_proba(client_info)
+        client_default_proba = client_proba[:, 1]
+
+        return {'proba_default': client_default_proba.tolist()}
+    except BaseException as e:
+        my_logger.error('Error while trying to predict dafault proba for all clients: ' + str(e))
+
+
+@app.get('/numeric_features_list')
+def get_numeric_features():
+    try:
+        numeric_features = classifier[0].named_transformers_['pipeline-3'][0].feature_names_in_
+        return {'numeric_features': numeric_features.tolist()}
+    except BaseException as e:
+        my_logger.error('Error while trying to retrieve numeric features as a list: ' + str(e))
+
+
+@app.get('/shap_values_default')
+def get_shap_values(client_id: int):
+    try:
+        client_info = client_database.loc[[client_id]]
+        explainer = shap.TreeExplainer(classifier[-1], model_outpout='predict_proba')
+
+        binary_features = classifier[0].named_transformers_['pipeline-1'][0].feature_names_in_
+        dummies_features = classifier[0].named_transformers_['pipeline-2'][0].get_feature_names()
+        numeric_features = classifier[0].named_transformers_['pipeline-3'][0].feature_names_in_
+
+        features = [*binary_features, *dummies_features, *numeric_features]
+
+        client_info_transformed = classifier[0].transform(client_info)
+        shap_vals = explainer.shap_values(client_info_transformed)
+        expected_values = explainer.expected_value
+
+        return {"shap_values": json.dumps(shap_vals[1].tolist()),
+                "expected_values": expected_values[1],
+                "features": features}
+    except BaseException as e:
+        my_logger.error('Error while trying to compute shap values: ' + str(e))
+
+
+@app.get('/client_ids')
+def get_client_ids():
+    return {"ids": client_database.index.to_list()}
+
+
+@app.get('/nearest_neighbors_ids')
+def get_nearest_neighbors_ids(client_id: int, n_neighbors: int):
+    try:
+        transformed_data = nearest_neighbors[0].transform(client_database.loc[[client_id]])
+        indices = nearest_neighbors[1].kneighbors(transformed_data, n_neighbors=n_neighbors, return_distance=False)
+        neighbors_ids = client_database.iloc[indices[0].tolist()].index.tolist()
+
+        return {"nearest_neighbors_ids": neighbors_ids}
+    except BaseException as e:
+        my_logger.error('Error while trying to find nearest_neighbors: ' + str(e))
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+# uvicorn main:app --reload
--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,3 +1,142 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4f05fd676b551edc4689247829b0a7c8c38634040f4f991b1ce08224d6a38694
-size 6447
+import streamlit as st
+from functions import load_client_info, filter_dataset, reload_scatter_plot, initialize_dashboard, \
+    build_gauge, reset_filter
+
+
+st.set_page_config(layout="wide", initial_sidebar_state='collapsed')
+
+
+def main():
+    initialize_dashboard()
+
+    st.markdown("""
+                <style>
+                div[data-testid='metric-container'] {
+                border: 1px solid rgba(28, 131, 225, 0.1);
+                border-radius: 5px;
+                padding: 5% 5% 5% 10%;
+                background-color: rgba(28, 131, 225, 0.1);
+                }
+                </style>""", unsafe_allow_html=True)
+
+    if "load_state" not in st.session_state:
+        st.session_state.load_state = False
+
+    title_col1, title_col2 = st.columns([0.7, 0.3])
+    title_col1.title('Dashboard Client ')
+    with title_col2.container():
+        placeholder = st.empty()
+
+    with st.form("Form"):
+        st.selectbox("Select an ID", st.session_state.ids, key='client_select_box')
+        btn = st.form_submit_button("Submit", on_click=load_client_info,
+                                    kwargs={'client_id': st.session_state.client_select_box})
+
+    tab1, tab2, tab3, tab4 = st.tabs(['Score', 'Info perso', 'Comparer', 'Historique demandes API'])
+
+    with tab1:
+        if btn or st.session_state.load_state:
+            st.session_state.load_state = True
+            placeholder.header(':open_file_folder: :blue[Dossier : ' + str(st.session_state.selected_client) + ']')
+            if st.session_state.missing_id:
+                st.write('Id not found in database :ghost:')
+            else:
+                if st.session_state.prediction == 'Crédit accordé':
+                    st.balloons()
+                    st.markdown("<h1 style='text-align: center; font-family:Corbel Light;'> Crédit<span "
+                                "style='color:#09ab3b'> accordé </span></h1>",
+                                unsafe_allow_html=True)
+                else:
+                    st.markdown("<h1 style='text-align: center; font-family:Corbel Light;'> Crédit<span "
+                                "style='color:#f63366'> refusé </span></h1>",
+                                unsafe_allow_html=True)
+                st.divider()
+                st.subheader('Détails')
+                tab1_col1, tab1_col2 = st.columns([0.3, 0.7])
+                if st.session_state.prediction == 'Crédit accordé':
+                    tab1_col1.plotly_chart(build_gauge('green'), use_container_width=True)
+                else:
+                    tab1_col1.plotly_chart(build_gauge('#f63366'), use_container_width=True)
+
+                tab1_col1.metric('Type de crédit', st.session_state.contract_type)
+                tab1_col2.pyplot(st.session_state.waterfall_plot, use_container_width=True)
+
+                tab1_col1_1, tab1_col1_2 = tab1_col1.columns(2)
+                tab1_col1_1.metric('Montant du crédit', st.session_state.amt_credit)
+                tab1_col1_2.metric('Montant des annuités', st.session_state.amt_annuity)
+
+    with tab2:
+        if btn or st.session_state.load_state:
+            if st.session_state.missing_id:
+                st.write('Identifiant non trouvé dans la base de données :ghost:')
+            else:
+                tab2_col1, tab2_col2 = st.columns([0.6, 0.4])
+                st.write("""<style>
+                            #[data-testid="stMetricDelta"] svg {display: none;}
+                            #</style>
+                            #""", unsafe_allow_html=True)
+                tab2_col1.subheader('Informations personnelles')
+
+                tab2_col1_1, tab2_col1_2, tab2_col1_3 = tab2_col1.columns(3)
+
+                tab2_col1_1.metric('Genre', st.session_state.gender)
+                tab2_col1_2.metric('Age', st.session_state.age)
+                tab2_col1_3.metric('Nombre d\'enfants', st.session_state.kids)
+
+                tab2_col1.metric('Situation familiale', st.session_state.family)
+
+                tab2_col1_11, tab2_col1_12 = tab2_col1.columns(2)
+                tab2_col1_11.metric('Emploi', st.session_state.occupation)
+                tab2_col1_12.metric('Revenu total', st.session_state.income)
+
+                # delta='revenu client moyen ' + str(st.session_state.income_mean)
+                tab2_col1.metric('Logement', st.session_state.housing)
+                tab2_col1.metric('Education', st.session_state.education)
+
+                # tab2_col1.metric('Type de revenus', st.session_state.income_type)
+
+                tab2_col2.subheader('Distributions')
+                tab2_col2.pyplot(st.session_state.donut_gender, use_container_width=True)
+                with tab2_col2.container():
+                    st.divider()
+                tab2_col2.pyplot(st.session_state.hist_age, use_container_width=True)
+
+    with tab3:
+        with st.form("Form1"):
+            st.selectbox("Feature X", st.session_state.numeric_features, key='x_var')
+            st.selectbox("Feature Y", st.session_state.numeric_features, key='y_var')
+
+            btn_var = st.form_submit_button("Submit", on_click=reload_scatter_plot)
+
+        with st.expander('Filtrer les données'):
+            with st.container():
+                st.write('Par dossiers clients similaires')
+                st.number_input('Enter a number of neighbors',
+                                min_value=1, max_value=st.session_state.number_of_clients - 1,
+                                value=10,
+                                key='n_neighbors')
+
+            with st.container():
+                st.write('Par Genre')
+                st.checkbox('M', value=True, key='m')
+                st.checkbox('F', value=True, key='f')
+
+            with st.container():
+                st.write('Par Age')
+                st.slider('Select a range of values', 10, 100, (10, 100), key='age_slider')
+
+            tab3_col1, tab3_col2 = st.columns([0.13, 0.87])
+            with tab3_col1.container():
+                btn_filter = st.button('Apply Filter', on_click=filter_dataset)
+            with tab3_col2.container():
+                btn_remove_filter = st.button('Remove Filter', on_click=reset_filter)
+
+        if btn_var or btn_filter or btn_remove_filter:
+            st.plotly_chart(st.session_state.scatter, use_container_width=True)
+
+    with tab4:
+        st.dataframe(st.session_state.requests_history, hide_index=True, use_container_width=True)
+
+
+if __name__ == '__main__':
+    main()
