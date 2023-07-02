7039550ee6704b06e84830e0ac977376f41900b1
Hermine Assossou <hermineassossou@gmail.com>
2023-07-03 00:43:17 +0200

    get working directory path 3

--- api/app/main.py
+++ api/app/main.py
@@ -49,8 +49,9 @@ def load_models():
     print(os.listdir('/code'))
     print(os.listdir('/code/models'))
     print(os.listdir('/code/data'))
+    print(os.listdir('/code/app'))
     try:
-        classifier = dill.load(open(MODEL_PATH + "/classifier.pkl", "rb"))
+        classifier = dill.load(open(${WEBAPP_STORAGE_HOME} + "models/classifier.pkl", "rb"))
         nearest_neighbors = dill.load(open(MODEL_PATH + "/nearest_neighbors.pkl", "rb"))
         print('Models loaded')
         # my_logger.info('Models loaded')
